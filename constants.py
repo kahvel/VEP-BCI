57f9e2e34c15ea2cf113e4d9f7cf9d5523f1dcaf
Anti Ingel <antiingel@gmail.com>
2015-04-05 02:41:29 +0300

    Add constants.py

---
+++ constants.py
@@ -0,0 +1,120 @@
+__author__ = 'Anti'
+
+MAIN_NOTEBOOK = "MainNotebook"
+PLOT_NOTEBOOK = "Plot"
+TARGETS_NOTEBOOK = "Targets"
+EXTRACTION_NOTEBOOK = "Extraction"
+
+WINDOW_TAB = "Window"
+TARGETS_TAB = "Targets"
+PLOT_TAB = "Plot"
+EXTRACTION_TAB = "Extraction"
+RECORD_TAB = "Record"
+RESULTS_TAB = "Results"
+TEST_TAB = "Test"
+
+TARGETS_TAB_TAB = "TargetsTab"
+PLOT_TAB_TAB = "PlotTab"
+EXTRACTION_TAB_TAB = "ExtractionTab"
+
+TEST_TIME = "Time"
+TEST_MIN = "Min"
+TEST_MAX = "Max"
+TEST_STANDBY = "Standby"
+TEST_TARGET = "Test target"
+TEST_UNLIMITED = "Unlimited"
+TEST_NONE = "None"
+TEST_RANDOM = "Random"
+
+WINDOW_WIDTH = "Width"
+WINDOW_HEIGHT = "Height"
+WINDOW_COLOR = "Color"
+WINDOW_FREQ = "Freq"
+WINDOW_REFRESH = "Refresh"
+WINDOW_MONITOR = "Monitor"
+
+RESULT_SHOW = "Show"
+RESULT_RESET = "Reset"
+
+RECORD_LENGTH = "Length"
+RECORD_NEUTRAL = "Neutral"
+RECORD_TARGET = "Target"
+RECORD_THRESHOLD = "Threshold"
+
+WINDOW_NONE = "None"
+WINDOW_HANNING = "Hanning"
+WINDOW_HAMMING = "Hamming"
+WINDOW_BLACKMAN = "Blackman"
+WINDOW_KAISER = "Kaiser"
+WINDOW_BARTLETT = "Bartlett"
+
+OPTIONS_NORMALISE = "Normalise"
+OPTIONS_DETREND = "Detrend"
+OPTIONS_FILTER = "Filter"
+OPTIONS_STEP = "Step"
+OPTIONS_LENGTH = "Length"
+OPTIONS_FROM = "From"
+OPTIONS_TO = "To"
+OPTIONS_TAPS = "Taps"
+OPTIONS_ARG = "Arg"
+OPTIONS_WINDOW = "Window"
+OPTIONS_BREAK = "Break"
+
+EXTRACTION_TAB_BUTTON_FRAME = "ExtractionTabButtonFrame"
+PLOT_TAB_BUTTON_FRAME = "PlotTabButtonFrame"
+
+MAIN_FRAME = "MainFrame"
+BOTTOM_FRAME = "BottomFrame"
+
+PLUS_MINUS_FRAME = "PlusMinusFrame"
+PLUS_MINUS_TEXTOX_FRAME = "PlusMinusTextboxFrame"
+COLOR_TEXTBOX_FRAME = "ColorTextboxFrame"
+DISABLE_DELETE_FRAME = "DisableDeleteFrame"
+
+TARGET_FRAME = "TargetFrame"
+
+TEXTBOX = "Textbox"
+
+TARGET_FREQ = "Freq"
+TARGET_DELAY = "Delay"
+TARGET_WIDTH = "Width"
+TARGET_HEIGHT = "Height"
+TARGET_COLOR1 = "Color1"
+TARGET_X = "x"
+TARGET_Y = "y"
+TARGET_COLOR2 = "Color2"
+
+PLUS = "+"
+MINUS = " -"
+
+DISABLE = "Disable"
+DELETE = "Delete"
+
+START_BUTTON = "Start"
+STOP_BUTTON = "Stop"
+SAVE_BUTTON = "Save"
+LOAD_BUTTON = "Load"
+EXIT_BUTTON = "Exit"
+
+PSDA = "PSDA"
+SUM_PSDA = "Sum PSDA"
+CCA = "CCA"
+BOTH = "Both"
+SUM_BOTH = "Sum Both"
+
+SIGNAL = "Signal"
+SUM_SIGNAL = "Sum signal"
+AVG_SIGNAL = "Avg signal"
+SUM_AVG_SIGNAL = "Sum avg signal"
+POWER = "Power"
+SUM_POWER = "Sum power"
+AVG_POWER = "Avg power"
+SUM_AVG_POWER = "Sum avg power"
+
+ALL_TAB = "All"
+PLUS_TAB = "+"
+
+SENSORS_FRAME = "SensorFrame"
+OPTIONS_FRAME = "OptionsFrame"
+
+SENSORS = ["AF3", "F7", "F3", "FC5","T7", "P7", "O1", "O2", "P8", "T8", "FC6","F4", "F8", "AF4"]
