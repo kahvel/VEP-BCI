f7d81ff9c12210d8573db2cdf9d0932be71d1543
Anti Ingel <antiingel@gmail.com>
2015-12-25 18:05:51 +0200

    Move TargetIdentification to separate class.

--- src/PostOffice.py
+++ src/PostOffice.py
@@ -4,6 +4,7 @@ import constants as c
 from connections import MasterConnection, ConnectionPostOfficeEnd
 import Results
 import Training
+import TargetIdentification
 
 
 class PostOffice(object):
@@ -13,18 +14,11 @@ class PostOffice(object):
         self.options = None
         self.results = Results.Results()
         self.training = Training.Training()
+        self.target_identification = TargetIdentification.TargetIdentification(self.connections, self.results)
         self.standby_state = None
         self.standby_freq = None
         self.no_standby = None
-        self.recorded_signals = [None for _ in range(7)]
-        self.prev_results = []
-        self.prev_results_counter = {}
-        self.actual_results = []
-        self.actual_results_counter = {}
-        self.need_new_target = None
-        self.new_target_counter = None
         self.message_counter = None
-        self.differences = []
         self.waitConnections()
 
     def waitConnections(self):
@@ -62,87 +56,6 @@ class PostOffice(object):
                     print("Unknown message in PostOffice: " + str(message))
             message = self.main_connection.receiveMessagePoll(0.1)
 
-    def countCca(self, counted_freqs, results, weight):
-        counted_freqs[round(results[0][0], 2)] += weight
-        self.differences.append(results[0][1]-results[1][1])
-
-    def countSumPsda(self, counted_freqs, results, weight):
-        for harmonic in results:
-            if harmonic in weight:
-                self.countCca(counted_freqs, results[harmonic], weight[harmonic])
-
-    def countPsda(self, counted_freqs, results, weight):
-        for sensor in results:
-            if sensor in weight:
-                self.countSumPsda(counted_freqs, results[sensor], weight[sensor])
-
-    def countAll(self, results, target_freqs, weight):
-        counted_results = {round(freq, 2): 0 for freq in target_freqs}
-        for tab in results:
-            for method in results[tab]:
-                if tab in weight and method[0] in weight[tab]:
-                    if method[0] == c.CCA:
-                        self.countCca(counted_results, results[tab][method], weight[tab][method[0]])
-                    elif method[0] == c.SUM_PSDA:
-                        self.countSumPsda(counted_results, results[tab][method], weight[tab][method[0]])
-                    elif method[0] == c.PSDA:
-                        self.countPsda(counted_results, results[tab][method], weight[tab][method[0]])
-        return counted_results
-
-    def getDictKey(self, dict, value_arg):
-        for key, value in dict.items():
-            if value == value_arg:
-                return key
-
-    def handleFreqMessages(self, message, target_freqs, current_target):
-        results = message
-        target_freqs_dict = target_freqs
-        target_freqs = target_freqs_dict.values()
-        rounded_target_freqs = tuple(round(freq, 2) for freq in target_freqs)
-        if results is not None:
-            self.differences = []
-            counted_freqs = self.countAll(results, target_freqs, {6: {c.CCA: 1}, 5: {c.SUM_PSDA: {1.0: 0.5, c.RESULT_SUM: 0.5}}})
-            if all(map(lambda x: x > 0.1, self.differences)):
-                for freq in counted_freqs:
-                    self.prev_results_counter[freq] += counted_freqs[freq]
-                self.prev_results.append(counted_freqs)
-                if len(self.prev_results) > 1:
-                    for result in self.prev_results[0]:
-                        self.prev_results_counter[result] -= self.prev_results[0][result]
-                    del self.prev_results[0]
-                    f, m = max(self.prev_results_counter.items(), key=lambda x: x[1])
-                    if m >= 1.5:
-                        self.actual_results.append(f)
-                        self.actual_results_counter[f] += 1
-                        if len(self.actual_results) > 1:
-                            self.actual_results_counter[self.actual_results[0]] -= 1
-                            del self.actual_results[0]
-                            f1, m1 = max(self.actual_results_counter.items(), key=lambda x: x[1])
-                            max_freq = target_freqs[rounded_target_freqs.index(f1)]
-                            if m1 >= 1:
-                                # if max_freq == self.standby_freq:
-                                #     # self.connections.sendPlotMessage(self.standby_state and not self.no_standby)
-                                #     self.standby_state = not self.standby_state
-                                #     self.connections.sendTargetMessage(self.standby_state)
-                                #     winsound.Beep(2500, 100)
-                                #     self.prev_results = []
-                                if not self.standby_state or self.no_standby:
-                                    self.connections.sendTargetMessage(max_freq)
-                                    current = target_freqs_dict[current_target] if current_target in target_freqs_dict else None
-                                    if not self.results.isPrevResult(max_freq):
-                                        self.results.addResult(current, max_freq)
-                                        self.connections.sendRobotMessage(self.getDictKey(target_freqs_dict, max_freq))
-                                        print(self.differences, sum(self.differences))
-                                        if max_freq != current:
-                                            print("wrong", self.actual_results, self.actual_results_counter, self.prev_results_counter, current, f1)
-                                        else:
-                                            print("right", self.actual_results, self.actual_results_counter, self.prev_results_counter, current, f1)
-                                    if max_freq == current:
-                                        self.new_target_counter += 1
-                                        if self.new_target_counter > 0:
-                                            self.need_new_target = True
-                                            self.new_target_counter = 0
-
     def getTotalTime(self, unlimited, test_time):
         return float("inf") if unlimited else test_time
 
@@ -168,8 +81,7 @@ class PostOffice(object):
             if target is not None:
                 self.training.collect_expected_target(target, self.message_counter)
                 self.connections.sendTargetMessage(target)
-            self.need_new_target = False
-            self.new_target_counter = 0
+            self.target_identification.resetTargetVariables()
             message = self.startPacketSending(target_freqs, target, total_time)
             if message is not None:
                 return message
@@ -212,12 +124,8 @@ class PostOffice(object):
             return True
 
     def start(self):
-        self.prev_results = []
-        self.actual_results = []
-        rounded_target_freqs = tuple(round(freq, 2) for freq in self.options[c.DATA_FREQS].values())
-        self.prev_results_counter = {freq: 0 for freq in rounded_target_freqs}
-        self.actual_results_counter = {freq: 0 for freq in rounded_target_freqs}
         self.message_counter = 0
+        self.target_identification.resetResults(self.options[c.DATA_FREQS].values())
         self.connections.sendStartMessage()
         message = self.targetChangingLoop(
             self.options[c.DATA_TEST],
@@ -234,7 +142,7 @@ class PostOffice(object):
             self.training.collect_packet(message)
             self.connections.sendExtractionMessage(message)
             self.connections.sendPlotMessage(message)
-            self.handleFreqMessages(
+            self.target_identification.handleFreqMessages(
                 self.connections.receiveExtractionMessage(),
                 target_freqs,
                 current_target
@@ -246,7 +154,7 @@ class PostOffice(object):
             self.connections.sendTargetMessage(message)
 
     def startPacketSending(self, target_freqs, current_target, total_time):
-        while not self.need_new_target and self.message_counter < total_time:
+        while not self.target_identification.need_new_target and self.message_counter < total_time:
             main_message = self.main_connection.receiveMessageInstant()
             if main_message in c.ROBOT_COMMANDS:
                 self.connections.sendRobotMessage(main_message)
---
+++ src/TargetIdentification.py
@@ -0,0 +1,109 @@
+import constants as c
+
+
+class TargetIdentification(object):
+    def __init__(self, master_connection, results):
+        self.prev_results = []
+        self.prev_results_counter = {}
+        self.actual_results = []
+        self.actual_results_counter = {}
+        self.need_new_target = None
+        self.new_target_counter = None
+        self.differences = []
+        self.master_connection = master_connection
+        self.results = results
+
+    def resetTargetVariables(self):
+        self.need_new_target = False
+        self.new_target_counter = 0
+
+    def resetResults(self, freqs):
+        rounded_target_freqs = tuple(round(freq, 2) for freq in freqs)
+        self.prev_results = []
+        self.prev_results_counter = self.initialiseCounter(rounded_target_freqs)
+        self.actual_results = []
+        self.actual_results_counter = self.initialiseCounter(rounded_target_freqs)
+
+    def initialiseCounter(self, rounded_target_freqs):
+        return {freq: 0 for freq in rounded_target_freqs}
+
+    def countCca(self, counted_freqs, results, weight):
+        counted_freqs[round(results[0][0], 2)] += weight
+        self.differences.append(results[0][1]-results[1][1])
+
+    def countSumPsda(self, counted_freqs, results, weight):
+        for harmonic in results:
+            if harmonic in weight:
+                self.countCca(counted_freqs, results[harmonic], weight[harmonic])
+
+    def countPsda(self, counted_freqs, results, weight):
+        for sensor in results:
+            if sensor in weight:
+                self.countSumPsda(counted_freqs, results[sensor], weight[sensor])
+
+    def countAll(self, results, target_freqs, weight):
+        counted_results = {round(freq, 2): 0 for freq in target_freqs}
+        for tab in results:
+            for method in results[tab]:
+                if tab in weight and method[0] in weight[tab]:
+                    if method[0] == c.CCA:
+                        self.countCca(counted_results, results[tab][method], weight[tab][method[0]])
+                    elif method[0] == c.SUM_PSDA:
+                        self.countSumPsda(counted_results, results[tab][method], weight[tab][method[0]])
+                    elif method[0] == c.PSDA:
+                        self.countPsda(counted_results, results[tab][method], weight[tab][method[0]])
+        return counted_results
+
+    def getDictKey(self, dict, value_arg):
+        for key, value in dict.items():
+            if value == value_arg:
+                return key
+
+    def handleFreqMessages(self, message, target_freqs, current_target):
+        results = message
+        target_freqs_dict = target_freqs
+        target_freqs = target_freqs_dict.values()
+        rounded_target_freqs = tuple(round(freq, 2) for freq in target_freqs)
+        if results is not None:
+            self.differences = []
+            counted_freqs = self.countAll(results, target_freqs, {6: {c.CCA: 1}, 5: {c.SUM_PSDA: {1.0: 0.5, c.RESULT_SUM: 0.5}}})
+            if all(map(lambda x: x > 0.1, self.differences)):
+                for freq in counted_freqs:
+                    self.prev_results_counter[freq] += counted_freqs[freq]
+                self.prev_results.append(counted_freqs)
+                if len(self.prev_results) > 1:
+                    for result in self.prev_results[0]:
+                        self.prev_results_counter[result] -= self.prev_results[0][result]
+                    del self.prev_results[0]
+                    f, m = max(self.prev_results_counter.items(), key=lambda x: x[1])
+                    if m >= 1.5:
+                        self.actual_results.append(f)
+                        self.actual_results_counter[f] += 1
+                        if len(self.actual_results) > 1:
+                            self.actual_results_counter[self.actual_results[0]] -= 1
+                            del self.actual_results[0]
+                            f1, m1 = max(self.actual_results_counter.items(), key=lambda x: x[1])
+                            max_freq = target_freqs[rounded_target_freqs.index(f1)]
+                            if m1 >= 1:
+                                # if max_freq == self.standby_freq:
+                                #     # self.connections.sendPlotMessage(self.standby_state and not self.no_standby)
+                                #     self.standby_state = not self.standby_state
+                                #     self.connections.sendTargetMessage(self.standby_state)
+                                #     winsound.Beep(2500, 100)
+                                #     self.prev_results = []
+                                if not self.standby_state or self.no_standby:
+                                    self.master_connection.sendTargetMessage(max_freq)
+                                    current = target_freqs_dict[current_target] if current_target in target_freqs_dict else None
+                                    if not self.results.isPrevResult(max_freq):
+                                        self.results.addResult(current, max_freq)
+                                        self.master_connection.sendRobotMessage(self.getDictKey(target_freqs_dict, max_freq))
+                                        print(self.differences, sum(self.differences))
+                                        if max_freq != current:
+                                            print("wrong", self.actual_results, self.actual_results_counter, self.prev_results_counter, current, f1)
+                                        else:
+                                            print("right", self.actual_results, self.actual_results_counter, self.prev_results_counter, current, f1)
+                                    if max_freq == current:
+                                        self.new_target_counter += 1
+                                        if self.new_target_counter > 0:
+                                            self.need_new_target = True
+                                            self.new_target_counter = 0
