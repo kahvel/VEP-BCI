a469f383223da5bf3725cf71cb25171bbecf25f7
Anti Ingel <antiingel@gmail.com>
2016-08-12 04:41:18 +0300

    Add option menu for choosing whether to use recorded EEG or features.

--- src/gui/main_window/test_tab/TestTab.py
+++ src/gui/main_window/test_tab/TestTab.py
@@ -9,15 +9,16 @@ class TestTab(Frame.Frame):
         self.addChildWidgets((
             OptionMenu.RecordingChoosingMenu(self, c.TEST_TAB_EEG_SOURCE_OPTION_MENU, 0, 1, c.EEG_SOURCE_NAMES),
             Checkbutton.Checkbutton(self, c.TEST_TAB_ALLOW_REPEATING, 0, 3),
-            OptionMenu.TargetChoosingMenu(self, c.TEST_TAB_TARGET_OPTION_MENU, 1, 1, c.TEST_TARGET_OPTIONS, command=self.enableTimePerTarget, columnspan=2),
+            OptionMenu.OptionMenu(self, c.TEST_TAB_RECORDED_TYPE_OPTION_MENU, 1, 1, c.TEST_RECORDED_TYPE_NAMES),
             Checkbutton.Checkbutton(self, c.TEST_TAB_CLEAR_BUFFERS, 1, 3),
-            OptionMenu.TargetChoosingMenu(self, c.TEST_TAB_STANDBY, 2, 1, (c.TEST_TARGET_NONE,), columnspan=2),
+            OptionMenu.TargetChoosingMenu(self, c.TEST_TAB_TARGET_OPTION_MENU, 2, 1, c.TEST_TARGET_OPTIONS, command=self.enableTimePerTarget, columnspan=2),
             Checkbutton.Checkbutton(self, c.TEST_TAB_PROCESS_SHORT_SIGNALS, 2, 3),
-            Textboxes.LabelTextbox(self, c.TEST_TAB_TOTAL_TIME, 3, 0, command=int, default_value=1, default_disability=True, default_disablers=[c.TEST_TAB_UNLIMITED]),
-            Checkbutton.Checkbutton(self, c.TEST_TAB_UNLIMITED, 3, 2, columnspan=2, command=self.enableTotalTime, default_value=1),
-            Textboxes.LabelTextbox(self, c.TEST_TAB_TIME_PER_TARGET, 4, 0, command=int, default_value=9, default_disability=True, default_disablers=[c.TEST_TAB_TARGET_OPTION_MENU]),
-            Textboxes.LabelTextbox(self, c.TEST_TAB_TIME_PER_TARGET_PLUS_MINUS, 4, 2, command=int, allow_zero=True, default_value=2, default_disability=True, default_disablers=[c.TEST_TAB_TARGET_OPTION_MENU]),
-            Textboxes.ColorTextboxFrame(self, c.TEST_TAB_COLOR, 5, 0, default_value="#ffffb3"),
+            OptionMenu.TargetChoosingMenu(self, c.TEST_TAB_STANDBY, 3, 1, (c.TEST_TARGET_NONE,), columnspan=2),
+            Textboxes.LabelTextbox(self, c.TEST_TAB_TOTAL_TIME, 4, 0, command=int, default_value=1, default_disability=True, default_disablers=[c.TEST_TAB_UNLIMITED]),
+            Checkbutton.Checkbutton(self, c.TEST_TAB_UNLIMITED, 4, 2, columnspan=2, command=self.enableTotalTime, default_value=1),
+            Textboxes.LabelTextbox(self, c.TEST_TAB_TIME_PER_TARGET, 5, 0, command=int, default_value=9, default_disability=True, default_disablers=[c.TEST_TAB_TARGET_OPTION_MENU]),
+            Textboxes.LabelTextbox(self, c.TEST_TAB_TIME_PER_TARGET_PLUS_MINUS, 5, 2, command=int, allow_zero=True, default_value=2, default_disability=True, default_disablers=[c.TEST_TAB_TARGET_OPTION_MENU]),
+            Textboxes.ColorTextboxFrame(self, c.TEST_TAB_COLOR, 6, 0, default_value="#ffffb3"),
         ))
 
     def enableTotalTime(self):
