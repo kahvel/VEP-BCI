Fix number of histogram bins to 50. Increase maxfev. Fix standard ITR calculation for accuracy=0.

src/data_handling/biosemi_data_converter/matrixManipulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from training.itr_calculators import ItrCalculatorProb
 
 
 table = """[[ 51.    0.    18.  496.]
@@ -8,7 +9,10 @@ table = """[[ 51.    0.    18.  496.]
 """.strip()
 
 
-parsed_table = np.array(map(lambda x: map(lambda y: int(y.strip()), x.strip().split(".")), table.replace("[", "").replace("]","").strip(".").split(".\n")), dtype=np.float)
+# parsed_table = np.array(map(lambda x: map(lambda y: int(y.strip()), x.strip().split(".")), table.replace("[", "").replace("]","").strip(".").split(".\n")), dtype=np.float)
+
+parsed_table = np.array([[51,0,0,49],[0,48,0,52],[0,0,1,99],[0,0,0,0]], dtype=np.float)
+# parsed_table = np.array([[21,0,1,78],[0,22,2,76],[0,0,10,90],[0,0,0,0]], dtype=np.float)
 
 def getItrBitPerMin(P, recall):
     window_length = 1
@@ -41,15 +45,10 @@ def getItrBitPerTrial(P, N):
     else:
         return (np.log10(N)+P*np.log10(P)+(1-P)*np.log10((1-P)/(N-1)))/np.log10(2)
 
-def calculateRecall(confusion_matrix):
+def calculatePredictionProbability(confusion_matrix):
     if not isinstance(confusion_matrix, float):
         matrix_sum = confusion_matrix.sum()
-        return (sumWithoutLastColumn(confusion_matrix))/matrix_sum
-
-def sumWithoutLastColumn(confusion_matrix):
-    if not isinstance(confusion_matrix, float):
-        matrix_sum = confusion_matrix.sum()
-        return matrix_sum-confusion_matrix.sum(axis=0)[-1]
+        return (matrix_sum-confusion_matrix.sum(axis=0)[-1])/matrix_sum
 
 def calculateAccuracy(confusion_matrix):
     if not isinstance(confusion_matrix, float):
@@ -57,14 +56,28 @@ def calculateAccuracy(confusion_matrix):
 
 def calculateAccuracyIgnoringLastColumn(confusion_matrix):
     if not isinstance(confusion_matrix, float):
-        return np.trace(confusion_matrix)/(confusion_matrix.sum()-confusion_matrix.sum(axis=0)[-1])
+            return np.trace(confusion_matrix)/(confusion_matrix.sum()-confusion_matrix.sum(axis=0)[-1])
+
+
+calculator = ItrCalculatorProb.ItrCalculatorProb(
+    window_length = 1,
+    step = 0.125,
+    look_back_length = 1,
+    feature_maf_length = 1,
+    proba_maf_length = 1,
+    n_targets = 3
+)
+calculator.n_classes = 3
+
 
 def printConfusionMatrixData(confusion_matrix):
+    calculator.class_probas = (confusion_matrix.sum(1)/confusion_matrix.sum())[:-1]
     accuracy = calculateAccuracyIgnoringLastColumn(confusion_matrix)
-    recall = calculateRecall(confusion_matrix)
-    print getItrBitPerMin(accuracy, recall)
+    R = calculatePredictionProbability(confusion_matrix)
+    print getItrBitPerTrial(accuracy, 2), calculator.itrMiFromMatrix(confusion_matrix)/60*calculator.mdt(R)
+    print getItrBitPerMin(accuracy, R)
     print accuracy
     print confusion_matrix
-    print getItrOffline(accuracy, 225.0/sumWithoutLastColumn(confusion_matrix), 3)
+    print getItrOffline(accuracy, 225.0/confusion_matrix.sum(axis=0)[-1], 3)
 
 printConfusionMatrixData(parsed_table)

src/training/DistributionPlotter.py
@@ -22,6 +22,8 @@ class Plotter(object):
         self.n_features = len(self.data)
         self.n_classes = len(self.ordered_labels)
         self.bar_width = 0.02
+        # self.bins = int((current_max-current_min)/self.bar_width)
+        self.bins = 50
         self.colors = ["blue", "green", "red"]
         self.parameters = []
 
@@ -51,7 +53,7 @@ class Plotter(object):
                     current_max = np.max(data[:,i])
                     ax.set_xlim((self.min_feature, self.max_feature))
                     ax.set_ylim((y_min, y_max))
-                    ax.hist(data[:,i], bins=int((current_max-current_min)/self.bar_width), normed=False, color=self.colors[i])
+                    ax.hist(data[:,i], bins=self.bins, normed=False, color=self.colors[i])
                     ax.plot([self.mean_thresholds[i], self.mean_thresholds[i]], [y_min, y_max], color=self.colors[i])
                     ax.set_title(self.labels[i])
                 else:
@@ -113,7 +115,7 @@ class Plotter(object):
         plt.plot(points, 1-scipy.stats.skewnorm.cdf(points, *parameters))
 
     def fitTest(self, data, current_min, current_max, initial_guess):
-        hist, bin_edges = np.histogram(data, bins=int((current_max-current_min)/self.bar_width), density=True)
+        hist, bin_edges = np.histogram(data, bins=self.bins, density=True)
         bin_edges = self.moving_average(bin_edges, 2)
         return scipy.optimize.curve_fit(scipy.stats.norm.pdf, bin_edges, hist, p0=initial_guess)
 
@@ -127,7 +129,7 @@ class Plotter(object):
         return scipy.stats.skewnorm.pdf(y, alpha)
 
     def fitSkew2(self, data, current_min, current_max, initial_guess):
-        hist, bin_edges = np.histogram(data, bins=int((current_max-current_min)/self.bar_width), density=True)
+        hist, bin_edges = np.histogram(data, bins=self.bins, density=True)
         bin_edges = self.moving_average(bin_edges, 2)
         return scipy.optimize.curve_fit(self.func, bin_edges, hist, p0=initial_guess)
 
@@ -156,7 +158,7 @@ class Plotter(object):
                 current_min = np.min(features)
                 current_max = np.max(features)
                 # print current_max, current_min, (current_max-current_min), (current_max-current_min)/0.005
-                plt.hist(features, bins=int((current_max-current_min)/self.bar_width), normed=True)
+                plt.hist(features, bins=self.bins, normed=True)
                 self.plotSkew(features, current_min, current_max)
         plt.tight_layout()
 

src/training/ModelTrainer.py
@@ -199,20 +199,20 @@ class ModelTrainer(object):
 
     def calculateAccuracyIgnoringLastColumn(self, confusion_matrix):
         if not isinstance(confusion_matrix, float):
-            return np.trace(confusion_matrix)/(confusion_matrix.sum()-confusion_matrix.sum(axis=0)[-1])
+                return np.trace(confusion_matrix)/(confusion_matrix.sum()-confusion_matrix.sum(axis=0)[-1])
 
     def printConfusionMatrixData(self, confusion_matrix):
         accuracy = self.calculateAccuracyIgnoringLastColumn(confusion_matrix)
         prediction_probability = self.calculatePredictionProbability(confusion_matrix)
-        print "Predictions:"
+        # print "Predictions:"
         print str((confusion_matrix.sum()-confusion_matrix.sum(axis=0)[-1])) + "/" + str(confusion_matrix.sum())
-        print "Standard ITR:"
+        # print "Standard ITR:"
         print self.itr_calculator_prob.itrBitPerMin(accuracy, prediction_probability)
-        print "Mutual information ITR:"
+        # print "Mutual information ITR:"
         print self.itr_calculator_prob.itrMiFromMatrix(confusion_matrix)
-        print "Accuracy:"
+        # print "Accuracy:"
         print accuracy
-        print "MDT:"
+        # print "MDT:"
         print self.itr_calculator_prob.mdt(prediction_probability)
         print confusion_matrix
 
@@ -464,13 +464,13 @@ class ModelTrainer(object):
         all_data = np.concatenate(split_data, 0)
         all_labels = np.concatenate(split_labels, 0)
         plotter = DistributionPlotter.Plotter(all_data, all_labels, thresholds, label_order)
-        # plotter.plotPdf()
+        plotter.plotPdf()
         # plotter.plotCdf()
         # plotter.plotJointPdfs()
         # plotter.pair()
         # import time
         # matplotlib2tikz.save("C:\\Users\Anti\\Desktop\\PycharmProjects\\VEP-BCI\\file" + str(round(time.time())) + ".tex", draw_rectangles=True)
-        # plt.show()
+        plt.show()
 
         # dummy_model = CvCalibrationModel.TrainingModel()
         # dummy_model.setup(self.features_to_use, 1, self.recordings, [False])

src/training/curve_fitting/SkewNormal.py
@@ -18,7 +18,8 @@ class PdfCurve(CurveFitting.Curve):
         CurveFitting.Curve.__init__(self)
         bar_width = 0.02
         self.all_features = all_features
-        self.bins = int((np.max(all_features)-np.min(all_features))/bar_width)
+        # self.bins = int((np.max(all_features)-np.min(all_features))/bar_width)
+        self.bins = 50
         self.parameters = None
         hist, bin_edges = np.histogram(self.all_features, bins=self.bins, density=True)
         bin_edges = self.moving_average(bin_edges, 2)
@@ -44,7 +45,7 @@ class PdfCurve(CurveFitting.Curve):
         return scipy.stats.skewnorm.pdf(x, *self.parameters)
 
     def fitSkew(self, initial_guess):
-        return scipy.optimize.curve_fit(self.skew, self.x, self.y, p0=initial_guess)
+        return scipy.optimize.curve_fit(self.skew, self.x, self.y, p0=initial_guess, maxfev=5000)
 
     def fitCurve(self):
         self.parameters = self.fitSkew([0, np.mean(self.all_features), np.std(self.all_features)])[0]  # , 0, 1])

src/training/itr_calculators/AbstractCalculator.py
@@ -73,7 +73,7 @@ class ItrCalculator(object):
         elif a == 1:
             return np.log2(self.n_targets)
         elif a == 0:
-            return np.log2(self.n_targets)*np.log2(1.0/(self.n_targets-1))
+            return np.log2(self.n_targets)+np.log2(1.0/(self.n_targets-1))
         else:
             return np.log2(self.n_targets)+a*np.log2(a)+(1-a)*np.log2((1.0-a)/(self.n_targets-1))
 